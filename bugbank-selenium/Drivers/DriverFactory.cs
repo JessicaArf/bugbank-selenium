d8f0d133b88530a405b30c6c6d372c825b05f0ce
Jessica Arf <jessicalealarf8@gmail.com>
2025-04-22 20:08:30 -0300

    feat: adicionar classe DriverFactory para inicializar o ChromeDriver

---
+++ bugbank-selenium/Drivers/DriverFactory.cs
@@ -0,0 +1,22 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bugbank_selenium.Drivers
+{
+    public class DriverFactory
+    {
+        public static IWebDriver CreateChrome()
+        {
+            var options = new ChromeOptions();
+            options.AddArgument("start-maximized");
+
+            return new ChromeDriver(options);
+        }
+    }
+}
---
+++ bugbank-selenium/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace bugbank_selenium
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
