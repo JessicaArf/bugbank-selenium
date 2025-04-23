939c9cc311b539534b88344d62b9e82337b9783c
Jessica Arf <jessicalealarf8@gmail.com>
2025-04-22 21:03:24 -0300

    feat: adicionar RegisterPage com elementos do formulário e ações

---
+++ bugbank-selenium/Pages/RegisterPage.cs
@@ -0,0 +1,30 @@
+﻿using bugbank_selenium.Drivers;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bugbank_selenium.Pages
+{
+    public class RegisterPage
+    {
+        private readonly IWebDriver driver;
+
+        public RegisterPage(IWebDriver driver)          {
+            this.driver = driver;
+        }
+
+        private IWebElement BtnOpenRegisterForm => driver.FindElement(By.CssSelector("button_child"));
+        
+        public void acess()
+        {
+            driver.Navigate().GoToUrl("https://bugbank.netlify.app/#");
+            BtnOpenRegisterForm.Click();
+        }
+
+      
+    }
+ 
+}
--- bugbank-selenium/obj/Debug/net8.0/bugbank-selenium.AssemblyInfo.cs
+++ bugbank-selenium/obj/Debug/net8.0/bugbank-selenium.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("bugbank-selenium")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fcba07093918684e72db73fc4d28dccf2ac8bb7d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d8f0d133b88530a405b30c6c6d372c825b05f0ce")]
 [assembly: System.Reflection.AssemblyProductAttribute("bugbank-selenium")]
 [assembly: System.Reflection.AssemblyTitleAttribute("bugbank-selenium")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
